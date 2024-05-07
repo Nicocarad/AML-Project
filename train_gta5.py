70ac8135516723124b911af35b11e34e2be7cd46
Nicolò Caradonna <nicocarad@gmail.com>
2024-05-07 13:14:41 +0200

    update

--- train_gta5.py
+++ train_gta5.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python
 # -*- encoding: utf-8 -*-
 from model.model_stages import BiSeNet
-from cityscapes import CityScapes
+from GTA5 import GTA5
 import torch
-from torch.utils.data import DataLoader
+from torch.utils.data import Subset, DataLoader
 import logging
 import argparse
 import numpy as np
@@ -13,12 +13,13 @@ from utils import poly_lr_scheduler
 from utils import reverse_one_hot, compute_global_accuracy, fast_hist, per_class_iu
 from tqdm import tqdm
 import sys
+from sklearn.model_selection import train_test_split
 
 logger = logging.getLogger()
 
 
 def val(args, model, dataloader):
-    print('start val!')
+    print("start val!")
     with torch.no_grad():
         model.eval()
         precision_record = []
@@ -48,19 +49,18 @@ def val(args, model, dataloader):
             # label = colour_code_segmentation(np.array(label), label_info)
             precision_record.append(precision)
 
-
         precision = np.mean(precision_record)
         miou_list = per_class_iu(hist)
         miou = np.mean(miou_list)
-        print('precision per pixel for test: %.3f' % precision)
-        print('mIoU for validation: %.3f' % miou)
-        print(f'mIoU per class: {miou_list}')
+        print("precision per pixel for test: %.3f" % precision)
+        print("mIoU for validation: %.3f" % miou)
+        print(f"mIoU per class: {miou_list}")
 
         return precision, miou
 
 
 def train(args, model, optimizer, dataloader_train, dataloader_val):
-    writer = SummaryWriter(comment=''.format(args.optimizer))
+    writer = SummaryWriter(comment="".format(args.optimizer))
 
     scaler = amp.GradScaler()
 
@@ -68,10 +68,12 @@ def train(args, model, optimizer, dataloader_train, dataloader_val):
     max_miou = 0
     step = 0
     for epoch in range(args.num_epochs):
-        lr = poly_lr_scheduler(optimizer, args.learning_rate, iter=epoch, max_iter=args.num_epochs)
+        lr = poly_lr_scheduler(
+            optimizer, args.learning_rate, iter=epoch, max_iter=args.num_epochs
+        )
         model.train()
         tq = tqdm(total=len(dataloader_train) * args.batch_size)
-        tq.set_description('Current epoch %d, lr %f' % (epoch, lr))
+        tq.set_description("Current epoch %d, lr %f" % (epoch, lr))
         loss_record = []
         for i, (data, label) in enumerate(dataloader_train):
             data = data.cuda()
@@ -90,123 +92,138 @@ def train(args, model, optimizer, dataloader_train, dataloader_val):
             scaler.update()
 
             tq.update(args.batch_size)
-            tq.set_postfix(loss='%.6f' % loss)
+            tq.set_postfix(loss="%.6f" % loss)
             step += 1
-            writer.add_scalar('loss_step', loss, step)
+            writer.add_scalar("loss_step", loss, step)
             loss_record.append(loss.item())
         tq.close()
         loss_train_mean = np.mean(loss_record)
-        writer.add_scalar('epoch/loss_epoch_train', float(loss_train_mean), epoch)
-        print('loss for train : %f' % (loss_train_mean))
+        writer.add_scalar("epoch/loss_epoch_train", float(loss_train_mean), epoch)
+        print("loss for train : %f" % (loss_train_mean))
         if epoch % args.checkpoint_step == 0 and epoch != 0:
             import os
+
             if not os.path.isdir(args.save_model_path):
                 os.mkdir(args.save_model_path)
-            torch.save(model.module.state_dict(), os.path.join(args.save_model_path, 'latest.pth'))
+            torch.save(
+                model.module.state_dict(),
+                os.path.join(args.save_model_path, "latest.pth"),
+            )
 
         if epoch % args.validation_step == 0 and epoch != 0:
             precision, miou = val(args, model, dataloader_val)
             if miou > max_miou:
                 max_miou = miou
                 import os
+
                 os.makedirs(args.save_model_path, exist_ok=True)
-                torch.save(model.module.state_dict(), os.path.join(args.save_model_path, 'best.pth'))
-            writer.add_scalar('epoch/precision_val', precision, epoch)
-            writer.add_scalar('epoch/miou val', miou, epoch)
+                torch.save(
+                    model.module.state_dict(),
+                    os.path.join(args.save_model_path, "best.pth"),
+                )
+            writer.add_scalar("epoch/precision_val", precision, epoch)
+            writer.add_scalar("epoch/miou val", miou, epoch)
+
 
 def str2bool(v):
-    if v.lower() in ('yes', 'true', 't', 'y', '1'):
+    if v.lower() in ("yes", "true", "t", "y", "1"):
         return True
-    elif v.lower() in ('no', 'false', 'f', 'n', '0'):
+    elif v.lower() in ("no", "false", "f", "n", "0"):
         return False
     else:
-        raise argparse.ArgumentTypeError('Unsupported value encountered.')
+        raise argparse.ArgumentTypeError("Unsupported value encountered.")
 
 
 def parse_args():
     parse = argparse.ArgumentParser()
 
-    parse.add_argument('--mode',
-                       dest='mode',
-                       type=str,
-                       default='train',
+    parse.add_argument(
+        "--mode",
+        dest="mode",
+        type=str,
+        default="train",
     )
 
-    parse.add_argument('--backbone',
-                       dest='backbone',
-                       type=str,
-                       default='STDCNet813',
+    parse.add_argument(
+        "--backbone",
+        dest="backbone",
+        type=str,
+        default="STDCNet813",
     )
-    parse.add_argument('--pretrain_path',
-                      dest='pretrain_path',
-                      type=str,
-                      default='./checkpoints/STDCNet813M_73.91.tar', # Pretrained on ImageNet ---> incolla: /STDCNet813M_73.91.tar
+    parse.add_argument(
+        "--pretrain_path",
+        dest="pretrain_path",
+        type=str,
+        default="./checkpoints/STDCNet813M_73.91.tar",  # Pretrained on ImageNet ---> incolla: /STDCNet813M_73.91.tar
     )
-    parse.add_argument('--use_conv_last',
-                       dest='use_conv_last',
-                       type=str2bool,
-                       default=False,
+    parse.add_argument(
+        "--use_conv_last",
+        dest="use_conv_last",
+        type=str2bool,
+        default=False,
     )
-    parse.add_argument('--num_epochs',
-                       type=int, default=5,
-                       help='Number of epochs to train for')
-    parse.add_argument('--epoch_start_i',
-                       type=int,
-                       default=0,
-                       help='Start counting epochs from this number')
-    parse.add_argument('--checkpoint_step',
-                       type=int,
-                       default=5,
-                       help='How often to save checkpoints (epochs)')
-    parse.add_argument('--validation_step',
-                       type=int,
-                       default=5,
-                       help='How often to perform validation (epochs)')
-    parse.add_argument('--crop_height',
-                       type=int,
-                       default=512,
-                       help='Height of cropped/resized input image to modelwork')
-    parse.add_argument('--crop_width',
-                       type=int,
-                       default=1024,
-                       help='Width of cropped/resized input image to modelwork')
-    parse.add_argument('--batch_size',
-                       type=int,
-                       default=8,
-                       help='Number of images in each batch')
-    parse.add_argument('--learning_rate',
-                        type=float,
-                        default=0.01,
-                        help='learning rate used for train')
-    parse.add_argument('--num_workers',
-                       type=int,
-                       default=2,
-                       help='num of workers')
-    parse.add_argument('--num_classes',
-                       type=int,
-                       default=19,
-                       help='num of object classes (with void)')
-    parse.add_argument('--cuda',
-                       type=str,
-                       default='0',
-                       help='GPU ids used for training')
-    parse.add_argument('--use_gpu',
-                       type=bool,
-                       default=True,
-                       help='whether to user gpu for training')
-    parse.add_argument('--save_model_path',
-                       type=str,
-                       default="./saved_model",
-                       help='path to save model')
-    parse.add_argument('--optimizer',
-                       type=str,
-                       default='adam',
-                       help='optimizer, support rmsprop, sgd, adam')
-    parse.add_argument('--loss',
-                       type=str,
-                       default='crossentropy',
-                       help='loss function')
-
+    parse.add_argument(
+        "--num_epochs", type=int, default=5, help="Number of epochs to train for"
+    )
+    parse.add_argument(
+        "--epoch_start_i",
+        type=int,
+        default=0,
+        help="Start counting epochs from this number",
+    )
+    parse.add_argument(
+        "--checkpoint_step",
+        type=int,
+        default=5,
+        help="How often to save checkpoints (epochs)",
+    )
+    parse.add_argument(
+        "--validation_step",
+        type=int,
+        default=5,
+        help="How often to perform validation (epochs)",
+    )
+    parse.add_argument(
+        "--crop_height",
+        type=int,
+        default=512,
+        help="Height of cropped/resized input image to modelwork",
+    )
+    parse.add_argument(
+        "--crop_width",
+        type=int,
+        default=1024,
+        help="Width of cropped/resized input image to modelwork",
+    )
+    parse.add_argument(
+        "--batch_size", type=int, default=8, help="Number of images in each batch"
+    )
+    parse.add_argument(
+        "--learning_rate", type=float, default=0.01, help="learning rate used for train"
+    )
+    parse.add_argument("--num_workers", type=int, default=2, help="num of workers")
+    parse.add_argument(
+        "--num_classes", type=int, default=19, help="num of object classes (with void)"
+    )
+    parse.add_argument(
+        "--cuda", type=str, default="0", help="GPU ids used for training"
+    )
+    parse.add_argument(
+        "--use_gpu", type=bool, default=True, help="whether to user gpu for training"
+    )
+    parse.add_argument(
+        "--save_model_path",
+        type=str,
+        default="./saved_model",
+        help="path to save model",
+    )
+    parse.add_argument(
+        "--optimizer",
+        type=str,
+        default="adam",
+        help="optimizer, support rmsprop, sgd, adam",
+    )
+    parse.add_argument("--loss", type=str, default="crossentropy", help="loss function")
 
     return parse.parse_args()
 
@@ -218,39 +235,64 @@ def main():
     n_classes = args.num_classes
 
     mode = args.mode
-    
 
-    train_dataset = CityScapes('/content/Cityscapes/Cityscapes/Cityspaces', mode)
-    dataloader_train = DataLoader(train_dataset,
-                    batch_size=args.batch_size,
-                    shuffle=False,
-                    num_workers=args.num_workers,
-                    pin_memory=False,
-                    drop_last=True)
+    train_dataset = GTA5("/content/GTA5/GTA5/GTA5", mode)
 
-    val_dataset = CityScapes('/content/Cityscapes/Cityscapes/Cityspaces', mode='val')
-    dataloader_val = DataLoader(val_dataset,
-                       batch_size=1,
-                       shuffle=False,
-                       num_workers=args.num_workers,
-                       drop_last=False)
+    indexes = range(0, len(train_dataset))
+    splitting = train_test_split(
+        indexes,
+        train_size=0.8,
+        random_state=42,
+        stratify=train_dataset.data["label"],
+        shuffle=True,
+    )
+    train_indexes = splitting[0]
+    val_indexes = splitting[1]
+
+    train_dataset = Subset(train_dataset, train_indexes)
+    val_dataset = Subset(train_dataset, val_indexes)
+
+    dataloader_train = DataLoader(
+        train_dataset,
+        batch_size=args.batch_size,
+        shuffle=False,
+        num_workers=args.num_workers,
+        pin_memory=False,
+        drop_last=True,
+    )
+
+    val_dataset = GTA5("/content/GTA5/GTA5/Cityspaces", mode="val")
+    dataloader_val = DataLoader(
+        val_dataset,
+        batch_size=1,
+        shuffle=False,
+        num_workers=args.num_workers,
+        drop_last=False,
+    )
 
     ## model
-    model = BiSeNet(backbone=args.backbone, n_classes=n_classes, pretrain_model=args.pretrain_path, use_conv_last=args.use_conv_last)
+    model = BiSeNet(
+        backbone=args.backbone,
+        n_classes=n_classes,
+        pretrain_model=args.pretrain_path,
+        use_conv_last=args.use_conv_last,
+    )
 
     if torch.cuda.is_available() and args.use_gpu:
         model = torch.nn.DataParallel(model).cuda()
 
     ## optimizer
     # build optimizer
-    if args.optimizer == 'rmsprop':
+    if args.optimizer == "rmsprop":
         optimizer = torch.optim.RMSprop(model.parameters(), args.learning_rate)
-    elif args.optimizer == 'sgd':
-        optimizer = torch.optim.SGD(model.parameters(), args.learning_rate, momentum=0.9, weight_decay=1e-4)
-    elif args.optimizer == 'adam':
+    elif args.optimizer == "sgd":
+        optimizer = torch.optim.SGD(
+            model.parameters(), args.learning_rate, momentum=0.9, weight_decay=1e-4
+        )
+    elif args.optimizer == "adam":
         optimizer = torch.optim.Adam(model.parameters(), args.learning_rate)
     else:  # rmsprop
-        print('not supported optimizer \n')
+        print("not supported optimizer \n")
         return None
 
     ## train loop
@@ -258,13 +300,14 @@ def main():
     # final test
     val(args, model, dataloader_val)
 
+
 if __name__ == "__main__":
-    
+
     output_file = "output.txt"
     with open(output_file, "w") as f:
-        
+
         sys.stdout = f
-        
+
         main()
-        
+
         sys.stdout = sys.__stdout__
